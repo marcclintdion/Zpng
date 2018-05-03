e91ae45ea82c2dc9125ef1c75be73e73c7e259b9
Catid <mrcatid@gmail.com>
2018-05-03 01:07:37 -0700

    Clean up Windows projects and header comments

--- zpng.cpp
+++ zpng.cpp
@@ -1,16 +1,61 @@
+/** \file
+    \brief Zpng - Experimental Lossless Image Compressor
+    \copyright Copyright (c) 2018 Christopher A. Taylor.  All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of Zpng nor the names of its contributors may be
+      used to endorse or promote products derived from this software without
+      specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "zpng.h"
 
 #include "zstd/zstd.h"
 
-#include <malloc.h>
+#include <malloc.h> // calloc
 
 
 //------------------------------------------------------------------------------
 // Constants
 
+// Higher compression levels do not gain much but hurt speed
 static const int kCompressionLevel = 1;
 
+// This enabled some specialized versions for RGB and RGBA
 #define ENABLE_RGB_COLOR_FILTER
+
+// Header definitions
+#define ZPNG_HEADER_MAGIC 0xFBF8
+#define ZPNG_HEADER_OVERHEAD_BYTES 8
+
+// File format header
+struct ZPNG_Header
+{
+    uint16_t Magic;
+    uint16_t Width;
+    uint16_t Height;
+    uint8_t Channels;
+    uint8_t BytesPerChannel;
+};
 
 
 //------------------------------------------------------------------------------
--- zpng.h
+++ zpng.h
@@ -1,4 +1,55 @@
-#pragma once
+/** \file
+    \brief Zpng - Experimental Lossless Image Compressor
+    \copyright Copyright (c) 2018 Christopher A. Taylor.  All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of Zpng nor the names of its contributors may be
+      used to endorse or promote products derived from this software without
+      specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef CAT_ZPNG_H
+#define CAT_ZPNG_H
+
+/** \page Zpng - Experimental Lossless Image Compressor
+
+    It's much faster than PNG and compresses better for photographic images.
+    This compressor often takes less than 6% of the time of a PNG compressor
+    and produces a file that is 66% of the size.
+    It was written in about 600 lines of C code thanks to the Zstd library.
+
+    This library is similar to PNG in that the image is first filtered,
+    and then submitted to a data compressor.
+    The filtering step is a bit simpler and faster but somehow more effective
+    than the one used in PNG.
+    The data compressor used is Zstd, which makes it significantly faster
+    than PNG to compress and decompress.
+
+    Filtering:
+
+    (1) Reversible color channel transformation.
+    (2) Split each color channel into a separate color plane.
+    (3) Subtract each color value from the one to its left.
+*/
 
 #include <stdint.h>
 
@@ -6,6 +57,7 @@
 extern "C" {
 #endif
 
+// Buffer returned by library
 struct ZPNG_Buffer
 {
     // Pointer to data
@@ -15,33 +67,16 @@ struct ZPNG_Buffer
     unsigned Bytes;
 };
 
-struct ZPNG_Header
-{
-    uint16_t Magic;
-    uint16_t Width;
-    uint16_t Height;
-    uint8_t Channels;
-    uint8_t BytesPerChannel;
-};
-
-#define ZPNG_HEADER_MAGIC 0xFBF8
-#define ZPNG_HEADER_OVERHEAD_BYTES 8
-
+// Image data returned by the library
 struct ZPNG_ImageData
 {
     // Pixel data
     ZPNG_Buffer Buffer;
 
-    /*
-        This supports 1-2 bytes per channel.
-        And 1-4 channels per pixel.
-        There is no support for separate color planes with different sizes.
-    */
-
-    // Number of bytes for each color channel
+    // Number of bytes for each color channel (1-2)
     unsigned BytesPerChannel;
 
-    // Number of channels for each pixel
+    // Number of channels for each pixel (1-4)
     unsigned Channels;
 
     // Width in pixels of image
@@ -58,19 +93,41 @@ struct ZPNG_ImageData
 //------------------------------------------------------------------------------
 // API
 
-// Compress image into a buffer.
-// The returned buffer should be passed to ZPNG_Free().
+/**
+    ZPNG_Compress()
+
+    Compress image into a buffer.
+
+    The returned buffer should be passed to ZPNG_Free().
+
+    On success returns a valid data pointer.
+    On failure returns a null pointer.
+*/
 ZPNG_Buffer ZPNG_Compress(
     const ZPNG_ImageData* imageData
 );
 
-// Decompress image from a buffer.
-// The returned ZPNG_Buffer should be passed to ZPNG_Free().
+/*
+    ZPNG_Decompress()
+
+    Decompress image from a buffer.
+
+    The returned ZPNG_Buffer should be passed to ZPNG_Free().
+
+    On success returns a valid data pointer.
+    On failure returns a null pointer.
+*/
 ZPNG_ImageData ZPNG_Decompress(
     ZPNG_Buffer buffer
 );
 
-// Free buffer when done to avoid leaks
+/*
+    ZPNG_Free()
+
+    Free buffer when done to avoid leaks.
+
+    This will also set the buffer data pointer to null.
+*/
 void ZPNG_Free(
     ZPNG_Buffer* buffer
 );
@@ -79,3 +136,6 @@ void ZPNG_Free(
 #ifdef __cplusplus
 }
 #endif
+
+
+#endif // CAT_ZPNG_H
